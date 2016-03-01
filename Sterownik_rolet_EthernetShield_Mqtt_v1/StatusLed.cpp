e85a120af95108a958cdc4b92f9e1505ece7c0f1
cytrus77 <cytrus77@gmail.com>
2016-03-01 13:51:52 +0100

    Update StatusLed.cpp

--- Sterownik_rolet_EthernetShield_Mqtt_v1/StatusLed.cpp
+++ Sterownik_rolet_EthernetShield_Mqtt_v1/StatusLed.cpp
@@ -14,7 +14,57 @@ statusled::statusled(int pin, led_mode mode)
   m_pin    = pin;
   pinMode(m_pin, OUTPUT);
   digitalWrite(m_pin, m_state);
+  m_timer = 0;
 }
 
+void turnOn()
+{
+  if (m_state != ON)
+  {
+    m_state = ON;
+    digitalWrite(m_pin, m_state);
+  }
+}
+  
+void turnOff()
+{
+  if (m_state != OFF)
+  {
+    m_state = OFF;
+    digitalWrite(m_pin, m_state);
+  }
+}
 
+void setMode(led_mode mode)
+{
+  if (m_mode != mode)
+  {
+    m_mode = mode;
+  }
+}
 
+bool getState()
+{
+  return m_state;
+}
+
+void checkTimer()
+{
+  switch (m_mode)
+  {
+    case off:
+    
+    break;
+    
+    case offline:
+    
+    break;
+    
+    case online:
+    
+    break;
+    
+    default:
+    break;
+  }
+}
