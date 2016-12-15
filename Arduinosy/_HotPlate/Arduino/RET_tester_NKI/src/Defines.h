499be507183f852cde3445194af82677fa85e0dc
Cytrynowy <cytrus77@gmail.com>
2016-12-15 22:36:13 +0100

    NKI RS485 tester

--- Arduinosy/_HotPlate/Arduino/RET_tester_NKI/src/Defines.h
+++ Arduinosy/_HotPlate/Arduino/RET_tester_NKI/src/Defines.h
@@ -2,15 +2,13 @@
 
 #include <Ethernet.h>
 
-#define DEBUG 1
-#define RELAY_PIN 5
-#define MSG_BUFFER_SIZE 1
-#define SERIAL_TIMEOUT 20   // 20ms
-#define TIMER0_PERIOD  50000 // in us
-#define TIMER_COUNT_1S 1000000/TIMER0_PERIOD
-#define TIMER_COUNT_2S 2*TIMER_COUNT_1S
-#define TIMER_COUNT_5S 5*TIMER_COUNT_1S
-#define MAX_STRING_LENGTH 5
+#define MSG_BUFFER_SIZE   10
+#define SERIAL_TIMEOUT    20   // 20ms
+#define MAX_STRING_LENGTH 20
+#define TIMER0_PERIOD     50000 // in us
+#define TIMER_COUNT_1S    1000000/TIMER0_PERIOD
+#define TIMER_COUNT_2S    2*TIMER_COUNT_1S
+#define TIMER_COUNT_5S    5*TIMER_COUNT_1S
 
 struct MsgBuffer
 {
@@ -20,30 +18,14 @@ struct MsgBuffer
 
 enum ERs485State
 {
-  Rs485Defined = 1,
-  Rs485Loopback,
+  Rs485Loopback = 1,
   Rs485RxTest,
-  Rs485TxTest,
-  Rs485NoResponse
-};
-
-enum EShortCircuitState
-{
-  ShortCircuitOff = 1,
-  ShortCircuit2s,
-  ShortCircuit5s,
-  ShortCircuitPerm
-};
-
-enum ERelayState
-{
-  Off = 0,
-  On
+  Rs485TxTest
 };
 
 
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };   //physical mac address
-byte ip[] = { 192, 168, 0, 143 };                      // ip in lan (that's what you need to use in your browser. ("192.168.1.178")
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xBA, 0xCA };   //physical mac address
+byte ip[] = { 192, 168, 0, 144 };                      // ip in lan (that's what you need to use in your browser. ("192.168.1.178")
 byte gateway[] = { 192, 168, 0, 1 };                   // internet access via router
 byte subnet[] = { 255, 255, 255, 0 };                  //subnet mask
 EthernetServer server(80);                             //server port
@@ -51,6 +33,5 @@ String readString;
 volatile int timerCounter = 0;
 MsgBuffer msgBuffer;
 ERs485State rs485State;
-EShortCircuitState shortCircuitState;
 bool addRefeshSection = true;
 bool sendString = false;
--- Arduinosy/_HotPlate/Arduino/RET_tester_NKI/src/RET_tester_NKI.cpp
+++ Arduinosy/_HotPlate/Arduino/RET_tester_NKI/src/RET_tester_NKI.cpp
@@ -1,19 +1,7 @@
 #include <SPI.h>
 #include <string.h>
-#include <avr/wdt.h>
 #include "TimerOne.h"
 #include "Defines.h"
-
-void changeRelayState(ERelayState newState)
-{
-  static ERelayState state = On;
-
-  if (state != newState)
-  {
-    digitalWrite(RELAY_PIN, newState == On ? false : true);
-    state = newState;
-  }
-}
 
 void changeRs485Mode(ERs485State newState)
 {
@@ -21,91 +9,60 @@ void changeRs485Mode(ERs485State newState)
   addRefeshSection = true;
 }
 
-void changeShortCircuitState(EShortCircuitState newState)
-{
-  shortCircuitState = newState;
-
-  switch (shortCircuitState)
-  {
-    case ShortCircuit2s:
-    case ShortCircuit5s:
-      timerCounter = 1;
-      changeRelayState(On);
-      break;
-
-    case ShortCircuitPerm:
-      timerCounter = 0;
-      changeRelayState(On);
-      break;
-
-    case ShortCircuitOff:
-    default:
-      timerCounter = 0;
-      changeRelayState(Off);
-      break;
-  }
-
-  addRefeshSection = true;
-}
-
-float getDcVoltage()
-{
-  int sensorReading = analogRead(A0);
-  float dcVoltage = static_cast<float>(sensorReading)/1024*20;    // sensorValue/1024   - 1024=5V ->
-
-  return dcVoltage;
-}
-
 void serialWriteString()
 {
-  // if (sendString)
-  // {
-  //   char* string = msgBuffer.readString[msgBuffer.counter];
-  //
-  //   if (*string == 0x7F)
-  //   {
-  //     for (int i = 0; i < MAX_STRING_LENGTH; ++i)
-  //     {
-  //       Serial.write(*string);
-  //       if (*string == 0x7F) break;
-  //     }
-  //   }
-  //
-  //   sendString = false;
-  // }
+  if (sendString)
+  {
+    Serial.print(msgBuffer.readString[msgBuffer.counter]);
+    sendString = false;
+  }
 }
 
 void serialReadString()
 {
-  // static bool newString = false;
-  // static char stringCounter = 0;
-  //
-  // if (Serial.available())
-  // {
-  //   char received = Serial.read();
-  //
-  //   if (received == 0x7F)
-  //   {
-  //     if (!newString)
-  //     {
-  //       newString = true;
-  //       stringCounter = 0;
-  //       if (++msgBuffer.counter == MSG_BUFFER_SIZE) msgBuffer.counter = 0;
-  //     }
-  //     else
-  //     {
-  //       msgBuffer.readString[msgBuffer.counter][stringCounter] = received;
-  //       newString = false;
-  //       sendString = true;
-  //     }
-  //   }
-  //
-  //   if (newString)
-  //   {
-  //     msgBuffer.readString[msgBuffer.counter][stringCounter++] = received;
-  //     if (stringCounter >= MAX_STRING_LENGTH) newString = false;
-  //   }
-  // }
+  static bool newString = false;
+  static char stringCounter = 0;
+
+  while (Serial.available())
+  {
+    char received = Serial.read();
+
+    if (received == 0x7E)
+    {
+      if (!newString)
+      {
+        newString = true;
+        stringCounter = 0;
+        if (++msgBuffer.counter == MSG_BUFFER_SIZE) msgBuffer.counter = 0;
+      }
+      else
+      {
+        msgBuffer.readString[msgBuffer.counter][stringCounter++] = received;
+        newString = false;
+        sendString = true;
+
+        while (stringCounter < MAX_STRING_LENGTH)
+        {
+          msgBuffer.readString[msgBuffer.counter][stringCounter++] = 0;
+        }
+
+        break;
+      }
+    }
+
+    if (newString)
+    {
+      msgBuffer.readString[msgBuffer.counter][stringCounter++] = received;
+
+      if (stringCounter == (MAX_STRING_LENGTH-1))
+      {
+        msgBuffer.readString[msgBuffer.counter][stringCounter] = 0x7E;
+        newString = false;
+        sendString = true;
+        break;
+      }
+    }
+  }
 }
 
 void refreshSection(EthernetClient& client)
@@ -128,7 +85,7 @@ void refreshSection(EthernetClient& client)
 
 void TimerLoop()
 {
-  if (timerCounter) ++timerCounter;
+  ++timerCounter;
 }
 
 void setup()
@@ -139,53 +96,20 @@ void setup()
   {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
-Serial.println("START 1");
   Serial.setTimeout(SERIAL_TIMEOUT);
+
   // start the Ethernet connection and the server:
   Ethernet.begin(mac, ip, gateway, subnet);
   server.begin();
-  Serial.println("START 2");
-
-  pinMode(RELAY_PIN, OUTPUT);
-  changeRelayState(Off);
-  changeShortCircuitState(ShortCircuitOff);
-  changeRs485Mode(Rs485Loopback);
-  Serial.println("START 3");
 
   Timer1.initialize(TIMER0_PERIOD);
   Timer1.attachInterrupt(TimerLoop);
-  Serial.println("START END");
 
+  changeRs485Mode(Rs485Loopback);
 }
 
 void loop()
 {
-  Serial.println("LOOP");
-
-  switch (shortCircuitState)
-  {
-  case ShortCircuit2s:
-    if (timerCounter > TIMER_COUNT_2S)
-    {
-      changeRelayState(Off);
-      changeShortCircuitState(ShortCircuitOff);
-    }
-    break;
-
-  case ShortCircuit5s:
-    if (timerCounter > TIMER_COUNT_5S)
-    {
-      changeRelayState(Off);
-      changeShortCircuitState(ShortCircuitOff);
-    }
-    break;
-
-  case ShortCircuitOff:
-  case ShortCircuitPerm:
-  default:
-    break;
-  }
-
   switch (rs485State)
   {
     case Rs485Loopback:
@@ -194,18 +118,17 @@ void loop()
       break;
 
     case Rs485RxTest:
-      Serial.write("~!Elo$75~");
+      if (TIMER_COUNT_1S < timerCounter)
+      {
+        Serial.write("~!EloHello$75~");
+        timerCounter = 0;
+      }
       break;
 
     case Rs485TxTest:
       serialReadString();
       break;
 
-    case Rs485NoResponse:
-      serialReadString();
-      break;
-
-    case Rs485Defined:
     default:
       break;
   }
@@ -230,15 +153,9 @@ void loop()
          if (c == '\n')
          {
            //controls the Arduino if you press the buttons
-           if (readString.indexOf("?buttonShortOff") >0)           changeShortCircuitState(ShortCircuitOff);
-           else if (readString.indexOf("?buttonShort2s") >0)       changeShortCircuitState(ShortCircuit2s);
-           else if (readString.indexOf("?buttonShort5s") >0)       changeShortCircuitState(ShortCircuit5s);
-           else if (readString.indexOf("?buttonShortPerm") >0)     changeShortCircuitState(ShortCircuitPerm);
-           else if (readString.indexOf("?button485Defined") >0)    changeRs485Mode(Rs485Defined);
-           else if (readString.indexOf("?button485Loopback") >0)   changeRs485Mode(Rs485Loopback);
+           if (readString.indexOf("?button485Loopback") >0)        changeRs485Mode(Rs485Loopback);
            else if (readString.indexOf("?button485RxTest") >0)     changeRs485Mode(Rs485RxTest);
            else if (readString.indexOf("?button485TxTest") >0)     changeRs485Mode(Rs485TxTest);
-           else if (readString.indexOf("?button485NoResponse") >0) changeRs485Mode(Rs485NoResponse);
 
            //clearing string for next read
            readString="";
@@ -259,28 +176,13 @@ void loop()
            client.println("<H1>RS-485 - RET tester</H1>");
            client.println("<hr />");
            client.println("<br />");
-           client.println("<H2>DC Short circuit test</H2>");
-           client.println("<a href=\"/?buttonShortOff\"\">1. No short circuit</a>");
-           client.println("<a href=\"/?buttonShort2s\"\">2. Short circuit for 2s</a>");
-           client.println("<a href=\"/?buttonShort5s\"\">3. Short circuit for 5s</a>");
-           client.println("<a href=\"/?buttonShortPerm\"\">4. Permanent short circuit</a><br />");
-           client.println("<br />");
            client.println("<H2>RS-485 mode</H2>");
-           client.println("<a href=\"/?button485Defined\"\">1. Defined response</a>");
-           client.println("<a href=\"/?button485Loopback\"\">2. RS-485 Loopback</a>");
-           client.println("<a href=\"/?button485RxTest\"\">3. RS-485 RX Test</a>");
-           client.println("<a href=\"/?button485TxTest\"\">4. RS-485 TX Test</a>");
-           client.println("<a href=\"/?button485NoResponse\"\">5. No response</a><br />");
+           client.println("<a href=\"/?button485Loopback\"\">1. RS-485 Loopback</a>");
+           client.println("<a href=\"/?button485RxTest\"\">2. RS-485 RX Test</a>");
+           client.println("<a href=\"/?button485TxTest\"\">3. RS-485 TX Test</a>");
            client.println("<br />");
            client.println("<H2>RET Status</H2>");
            // DC Voltage section
-           client.print("<a style=\"background-color:#4DB257\">DC Voltage = ");  //
-           client.print(getDcVoltage());
-           client.println("V </a>");
-           // Short circuit section
-           client.print("<a style=\"background-color:#4DB257\">Short circuit mode: ");  //
-           client.print(shortCircuitState);
-           client.println("</a>");
            // RS485 section
            client.print("<a style=\"background-color:#4DB257\">RS-485 mode: ");  //
            client.print(rs485State);
@@ -291,13 +193,13 @@ void loop()
            client.print(MSG_BUFFER_SIZE);
            client.println(" received messages</H2>");
 
-          //  char counter = msgBuffer.counter;
-          //  for(char i = 0; i < MSG_BUFFER_SIZE; ++i)
-          //  {
-          //    client.print(msgBuffer.readString[counter]);
-          //    client.println("<br />");
-          //    if (--counter < 0) counter = MSG_BUFFER_SIZE-1;
-          //  }
+           char counter = msgBuffer.counter;
+           for(char i = 0; i < MSG_BUFFER_SIZE; ++i)
+           {
+             client.print(msgBuffer.readString[counter]);
+             client.println("<br />");
+             if (--counter < 0) counter = MSG_BUFFER_SIZE-1;
+           }
 
            client.println("<br />");
            client.println("</BODY>");
